488321976829584b19cf69b8c32c3eea5f89dcc1
Kevin Kuhlmann <kevinkuhlmann0526@gmail.com>
2015-07-02 17:30:05 -0400

    Redid Rainforest application

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+
+
+
+  root 'products#index'
+
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :products do
+    resources :reviews, only: [:show, :create, :destroy]
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
